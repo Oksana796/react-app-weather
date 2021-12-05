ef88ac577a0ee68520b482dce0a460509be04389
Oksana <ksenia79460@gmail.com>
2021-12-05 13:35:40 +0000

    created new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
           This project was coded by Oksana and is{" "}
           <a
