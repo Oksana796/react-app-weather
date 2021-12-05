2e1bf5930b6133bcf41dcc2dd0683e5473f20d76
Oksana <ksenia79460@gmail.com>
2021-12-05 14:59:48 +0000

    add structure and start style

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,43 @@
 import React from "react";
+import "./Weather.css";
 
-export default function Weather() {
-  return <h2>Hi</h2>;
+export default function Weather(props) {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              className="form-search"
+              placeholder="Enter a city.."
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Sunday 14:40</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="cloudy"
+          />
+          7°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 51%</li>
+            <li>Wind: 0.45 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
