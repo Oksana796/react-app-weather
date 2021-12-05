59d3daaddb9a7e469a357f75b5747aee27ea95b8
Oksana <ksenia79460@gmail.com>
2021-12-05 13:29:23 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 //import logo from './logo.svg';
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <footer>
+          This project was coded by Oksana and is{" "}
+          <a
+            href="https://github.com/Oksana796/react-app-weather"
+            className="GitHub-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="https://react-app-weather-finish.netlify.app/">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
